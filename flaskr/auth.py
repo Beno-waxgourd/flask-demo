5486e14105b46b7914e51aebaa554598137b00a2
韩东辰 <947979144@qq.com>
2022-03-20 21:45:18 +0800

    1

---
+++ flaskr/auth.py
@@ -0,0 +1,95 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+# 这里创建了一个名称为 'auth' 的Blueprint。
+# 和应用对象一样，蓝图需要知道是在哪里定义的，因此把 __name__ 作为函数的第二个参数。 url_prefix会添加到所有与该蓝图关联的URL前面。
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # request.form 是一个特殊类型的 dict ，其映射了提交表单的键和值。
+        db = get_db()
+        cursor = db.cursor()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                cursor.execute("INSERT INTO users (username, password) VALUES ('" + username + "', '" + generate_password_hash(password) + "')")
+                # 使用 generate_password_hash() 生成安全的哈希值，再把哈希值储存到数据库中
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+                # 如果用户名已存在，会产生一个IntegrityError错误，应当将该错误作为一个验证错误显示给用户。
+            else:
+                return redirect(url_for("auth.login"))
+                # redirect()为生成的 URL 生成一个重定向响应。
+
+        flash(error)
+        # 如果验证失败，那么会向用户显示一个出错信息。 flash() 用于储存在渲染模块时可以调用的信息。
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        cursor = db.cursor()
+        error = None
+        cursor.execute("SELECT * from users WHERE username='" + username + "'AND password='" + password + "'")
+        users = cursor.fetchone()
+        # print(username,password,'users:',users)
+        # posts = cursor.fetchall()
+        # print("posts:",posts)
+        # cursor.execute(
+        #     'SELECT * FROM users WHERE username = ?', (username,))
+        # user = cursor.fetchone()
+        # fetchone() 根据查询返回一个记录行。如果查询没有结果，则返回 None。
+
+        if users is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(users[1], password):
+            # check_password_hash() 以相同的方式哈希提交的 密码并安全的比较哈希值。如果匹配成功，那么密码就是正确的。
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            # session是一个 dict, 它用于储存横跨请求的值。当验证成功后，用户的 id 被储存于一个新的会话中。
+            # 会话数据被储存到一个 向浏览器发送的 cookie 中，在后继请求中，浏览器会返回它。
+            session["user_id"] = users[0]
+            # session['user_id'] = users['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.users is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
---
+++ flaskr/db.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import psycopg2
+import click
+from flask import current_app, g
+# g 是一个特殊对象，独立于每一个请求。
+# 在处理请求过程中，它可以用于储存可能多个函数都会用到的数据。
+# 把连接储存于其中，可以多次使用，而不用在同一个 请求中每次调用 get_db 时都创建一个新的连接。
+from flask.cli import with_appcontext
+
+
+# def connectPostgreSQL():
+#     conn = psycopg2.connect(database="flask_demo", user='postgres', password='password', host='127.0.0.1', port='5432')
+#     print('connect successful')
+
+def get_db():
+    if 'db' not in g:
+        conn = psycopg2.connect(database="flask_demo", user='postgres', password='password', host='127.0.0.1', port='5432')
+        # print('connect success')
+        g.db = conn
+
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+
+    db = get_db()
+    cursor = db.cursor()
+
+    with current_app.open_resource('schema.sql') as f:
+        # open_resource() 打开一个文件，该文件名是相对于flaskr包的。
+        # print('open success')
+        # print(f.readline().decode('utf8'))
+        """
+        db.to_sql(f.read().decode('utf8'))
+        db.executescript(f.read().decode('utf8'))
+        """
+        cursor.execute(f.read().decode('utf8'))
+    db.commit()
+    print('exec success')
+
+
+@click.command('init-db')
+# click.command() 定义一个名为init-db命令行，它调用init_db函数，并为用户显示一个成功的消息。
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    # app.teardown_appcontext() 告诉Flask在返回响应后进行清理的时候调用此函数。
+    app.cli.add_command(init_db_command)
+    # app.cli.add_command() 添加一个新的可以与flask一起工作的命令。
+
+# if __name__ == '__main__':
+#
+#     # connectPostgreSQL()
+#
+#     conn = psycopg2.connect(database="flask_demo", user='postgres', password='password', host='127.0.0.1', port='5432')
+#     print('connect successful')
+#     cursor = conn.cursor()
+#     cursor.execute('''DROP TABLE IF EXISTS users;
+# DROP TABLE IF EXISTS post;
+#
+# CREATE TABLE users (
+#   id SERIAL PRIMARY KEY,
+#   username TEXT UNIQUE NOT NULL,
+#   password TEXT NOT NULL
+# );
+#
+# CREATE TABLE post (
+#   id SERIAL PRIMARY KEY,
+#   author_id INTEGER NOT NULL,
+#   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+#   title TEXT NOT NULL,
+#   body TEXT NOT NULL,
+#   FOREIGN KEY (author_id) REFERENCES users (id)
+# );''')
+#     print('Table created successfully')
+#     conn.commit()
+#     conn.close()
+
+
+
