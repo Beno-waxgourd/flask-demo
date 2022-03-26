8bb5273ff5e30e43e89372e9182cb6b7c1c4f259
韩东辰 <947979144@qq.com>
2022-03-26 22:16:13 +0800

    1

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -20,14 +20,19 @@ def index():
     # )
     # posts = cursor.fetchall()
     db = get_db()
-    # posts = db.execute(
-    #     """SELECT p.id, title, body, created, author_id, username
-    #     FROM post AS p JOIN users AS u ON p.author_id = u.id
-    #     ORDER BY created DESC"""
-    # ).fetchall()
-    posts = db.query(Post, Users).filter(Post.author_id == Users.id).order_by(Post.created).all()
-    for p,u in posts:
-        print(p.id, p.title, p.body, p.created, p.author_id, u.username)
+    posts = db.execute(
+        """SELECT p.id, title, body, created, author_id, username
+        FROM post AS p JOIN users AS u ON p.author_id = u.id
+        ORDER BY created DESC"""
+    ).fetchall()
+    # print(posts)
+
+    # p, u = db.query(Post, Users.username).filter(Post.author_id == Users.id).order_by(Post.created).all()[0]
+    # posts = [(p.id, p.title, p.body, p.created, p.author_id, u)]
+    # print(posts)
+    # posts = db.query(Post, Users).filter(Post.author_id == Users.id).order_by(Post.created).all()
+    # for p,u in posts:
+    #     print(p.id, p.title, p.body, p.author_id, u.username)
 
     return render_template('blog/index.html', posts=posts)
 
@@ -63,26 +68,28 @@ def get_post(id, check_author=True):
     # cursor.execute("""SELECT p.id, title, body, created, author_id, username FROM post p JOIN users u ON p.author_id = u.id WHERE p.id = %d""" %(id) )
     # post = cursor.fetchone()
     # post = db.execute("""SELECT p.id, title, body, created, author_id, username FROM post p JOIN users u ON p.author_id = u.id WHERE p.id = %d""" %(id) ).fetchone()
-    post = db.query(Post, Users.username).filter(Post.id == id).outerjoin(Users, Users.id == Post.author_id).fetchone()
+    post = db.query(Post, Users.username).filter(Post.id == id).outerjoin(Users, Users.id == Post.author_id).first()
+    p, u = post
+    print(p, u)
+    print(p.id, p.title, p.body, p.author_id, p.created, u)
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.users['id']:
+    if check_author and p.author_id != g.users['id']:
         abort(403)
     # abort() 会引发一个特殊的异常，返回一个 HTTP 状态码。
     # 404 表示“未找到”， 403 代表“禁止访问”。
-    return post
+    return p
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    post = get_post(id)
+    p = get_post(id)
 
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
         error = None
-
         if not title:
             error = 'Title is required.'
 
@@ -99,7 +106,7 @@ def update(id):
             db.commit()
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/update.html', post=post)
+    return render_template('blog/update.html', post=p)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
